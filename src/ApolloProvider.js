710012b95cf91a4a6fdb2e718167d0f9b83cd7f2
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-08-21 23:14:02 +0300

    feat: initial commit

---
+++ src/ApolloProvider.js
@@ -0,0 +1,21 @@
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const httpLink = createHttpLink({
+  uri: "https://kdame-gebeya-server.herokuapp.com/",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
