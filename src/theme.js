86e75a01303d5d840ae7e924f3b8a83770d6be8c
hannasamuel20 <hanna17st@gmail.com>
2020-09-14 16:52:38 +0300

    fix: unexpected token error

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,19 @@
 const { createMuiTheme } = require('@material-ui/core');
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-        // light: will be calculated from palette.primary.main,
-          main: '#3f51b5',
-        // dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
-        },
-        secondary: {
-          main: '#f5f5f5',
-          // dark: will be calculated from palette.secondary.main,
-          contrastText: '#ffcc00',
-        },
-      },
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#3f51b5',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#f5f5f5',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+  },
   typography: {
     subtitle2: {
       fontWeight: 600,
@@ -24,6 +24,4 @@ const theme = createMuiTheme({
   },
 });
 
-  
-});
 export default theme;
