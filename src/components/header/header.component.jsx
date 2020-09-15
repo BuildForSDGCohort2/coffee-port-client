284c98707b8df4ea87547f0f69c9b15c509adb8c
hannasamuel20 <hanna17st@gmail.com>
2020-09-15 15:35:43 +0300

    feat: added notification badge

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import {AppBar,Toolbar, Typography} from '@material-ui/core';
 import {Route,Link} from 'react-router-dom';
 import useStyles from './header.styles';
 import CustomToggleMenu from '../custom-toggle-menu/custom-toggle-menu.component';
+import NotificationBadge from '../notification-badge/notification-badge.component';
 
 
 
@@ -25,6 +26,7 @@ const Header=()=>{
                 <Link className={link} to="/buyer"> <Typography>Buyers</Typography> </Link>
                 :null}
                 <Link className={link} to="/signup"> <Typography>Signup</Typography> </Link>
+                <Link className={link} to="/notification"> <NotificationBadge/> </Link>
                 <div className={link} to="/signup"> <CustomToggleMenu/> </div>
            
 
---
+++ src/components/notification-badge/notification-badge.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+const NotificationBadge = ()=> {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Badge color="secondary" badgeContent={0}>
+        <NotificationsIcon />
+      </Badge>
+    </div>
+  );
+}
+
+export default NotificationBadge;
