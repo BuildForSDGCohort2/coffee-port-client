c697098918f1168aff14aab742e497d1c3fe8c4a
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-09-17 11:29:33 +0300

    feat: added supplier description page

---
+++ src/pages/single-supplier/single-supplier.styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    margin: '5% 12%',
+    width: '76%',
+  },
+  content: {
+    margin: '3% 10%',
+  },
+  title: {
+    margin: '2% 0%',
+  },
+  body: {
+    margin: '2% 0%',
+  },
+});
+export default useStyles;
